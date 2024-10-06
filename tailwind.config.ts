f0999ab6452bb0f27e39b1417040f642a0bf6273
〽️iraya <mirayaabrodi@gmail.com>
2024-10-06 19:45:20 +0200

    fix animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,14 +12,14 @@ module.exports = {
         primary: "#4E8EF3",
       },
       animation: {
-        shake: "shake 0.5s ease-in-out",
+        shake: "shake 0.3s ease-in-out",
       },
       keyframes: {
         shake: {
           "0%, 100%": { transform: "translateX(0)" },
-          "25%": { transform: "translateX(-5px)" },
-          "50%": { transform: "translateX(5px)" },
-          "75%": { transform: "translateX(-5px)" },
+          "25%": { transform: "translateX(-10px)" },
+          "50%": { transform: "translateX(10px)" },
+          "75%": { transform: "translateX(-10px)" },
         },
       },
     },
